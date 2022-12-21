8b790d03bb131fc7211670753ef382767b49358d
Francesco Limpias <francescolimpias@gmail.com>
2022-12-21 23:49:05 +0100

    Added base AppFooter style

--- src/components/AppFooter.vue
+++ src/components/AppFooter.vue
@@ -6,17 +6,50 @@
 
         <!-- Form: newsletter -->
         <div class="newsletter">
-            Newsletter to get in touch
+            <div class="title">
+                Newsletter to get in touch
+            </div>
+            <div class="form">
+                <input type="text" placeholder="Your e-mail ->">
+            </div>
         </div>
 
         <!-- Navbar -->
         <nav>
-            Navbar
+            <ul>
+                <li>
+                    Address
+                </li>
+            </ul>
         </nav>
+
+        <div class="rights">
+            <span class="info">
+                ©2020 Maxcoach, All Rights Reserved
+            </span>
+        </div>
 
     </div>
 </template>
 
 <style lang="scss" scoped>
-.AppFooter {}
+.AppFooter {
+    .newsletter {
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        gap: 2rem;
+    }
+
+    ul{
+        display: flex;
+        justify-content: center;
+        align-items: center;
+        list-style-type: none;
+    }
+
+    .rights{
+        text-align: center;
+    }
+}
 </style>
