2c43a4d620ade48ed321905b62c831e71c253527
Francesco Limpias <francescolimpias@gmail.com>
2022-12-21 11:52:32 +0100

    Added base AppHeader struct

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -3,12 +3,25 @@
 
 <template>
     <div class="AppHeader">
-        AppHeader
+
+        <!-- Tickets countdown -->
+        <div class="ticket">
+            Get Ticket
+        </div>
+
+        <!-- Navbar -->
+        <nav>
+            MaxCoach
+        </nav>
+
+        <!-- Hero -->
+        <div class="hero">
+            Hello, I'm Martin
+        </div>
+
     </div>
 </template>
 
 <style lang="scss" scoped>
-.AppHeader{
-    
-}
+.AppHeader {}
 </style>
